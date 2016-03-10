8426c71ae77e28a774f99ab4cd957fd18a7e510b
Keiho Sakapon <saka_pon@live.jp>
2016-03-10 19:31:21 +0900

    Implement File Splitter

--- FileSplitter/FileSplitter/Program.cs
+++ FileSplitter/FileSplitter/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileSplitter
 {
@@ -10,6 +9,46 @@ namespace FileSplitter
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1) return;
+
+            if (args.Length == 1)
+                Split(args[0]);
+            else
+                Split(args[0], int.Parse(args[1]));
+        }
+
+        static void Split(string sourceFilePath, int maxSizeInMegabyte = 1024)
+        {
+            using (var source = File.OpenRead(sourceFilePath))
+            using (var reader = new BinaryReader(source))
+            {
+                for (var i = 0; ; i++)
+                {
+                    var targetFilePath = $"{sourceFilePath}.{i:D3}";
+
+                    using (var target = File.Create(targetFilePath))
+                    using (var writer = new BinaryWriter(target))
+                    {
+                        CopyBytes(reader, writer, maxSizeInMegabyte);
+                    }
+
+                    if (source.Position >= source.Length) break;
+                }
+            }
+        }
+
+        static void CopyBytes(BinaryReader reader, BinaryWriter writer, int maxSizeInMegabyte)
+        {
+            var buffer = new byte[1024 * 1024];
+            var readSize = 0;
+
+            for (var i = 0; i < maxSizeInMegabyte; i++)
+            {
+                readSize = reader.Read(buffer, 0, buffer.Length);
+                writer.Write(buffer, 0, readSize);
+
+                if (readSize < buffer.Length) break;
+            }
         }
     }
 }
