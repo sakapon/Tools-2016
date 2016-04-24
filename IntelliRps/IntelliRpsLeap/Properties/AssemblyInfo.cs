1e7465cf420d603c54897f29ba5dce51ee6d0db4
Keiho Sakapon <saka_pon@live.jp>
2016-04-24 13:24:44 +0900

    Update project settings

--- IntelliRps/IntelliRpsLeap/Properties/AssemblyInfo.cs
+++ IntelliRps/IntelliRpsLeap/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("IntelliRps for Leap Motion")]
+[assembly: AssemblyTitle("Intelli-RPS for Leap Motion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Keiho Sakapon")]
