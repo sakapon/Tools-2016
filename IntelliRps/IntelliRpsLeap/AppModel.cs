83592cb0065cd02895070b1bd570e7fbd045d3f8
Keiho Sakapon <saka_pon@live.jp>
2016-04-24 12:55:21 +0900

    Add classes

---
+++ IntelliRps/IntelliRpsLeap/AppModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+
+namespace IntelliRpsLeap
+{
+    public class AppModel
+    {
+        public LeapManager LeapManager { get; } = new LeapManager();
+
+        public ReadOnlyReactiveProperty<int?> ExtendedFingersCount { get; }
+
+        public AppModel()
+        {
+            ExtendedFingersCount = LeapManager.FrameArrived
+                .Select(f => f.Hands.Frontmost)
+                .Select(h => h.IsValid ? h.Fingers.Count(f => f.IsExtended) : default(int?))
+                .ToReadOnlyReactiveProperty();
+        }
+    }
+}
