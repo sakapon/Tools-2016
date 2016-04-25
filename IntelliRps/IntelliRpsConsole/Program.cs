a7ec745f4c713413e11cbe92952125c14b27fca2
Keiho Sakapon <saka_pon@live.jp>
2016-04-25 18:40:04 +0900

    Implement console app

--- IntelliRps/IntelliRpsConsole/Program.cs
+++ IntelliRps/IntelliRpsConsole/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using IntelliRps;
 
 namespace IntelliRpsConsole
 {
@@ -10,6 +9,49 @@ namespace IntelliRpsConsole
     {
         static void Main(string[] args)
         {
+            var game = new Game();
+
+            Console.WriteLine("Press [1][2][3] keys to play.");
+            Console.WriteLine("Press [Enter] key to exit.");
+            Console.WriteLine();
+
+            while (true)
+            {
+                var consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.Enter) return;
+                switch (consoleKey.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.D2:
+                    case ConsoleKey.D3:
+                        break;
+                    default:
+                        continue;
+                }
+
+                var playerShape = (Shape)(int.Parse(consoleKey.KeyChar.ToString()) - 1);
+                var computerShape = game.NextComputerShape();
+                game.AddMatchInfo(playerShape, computerShape);
+
+                Console.Write(" ");
+                Console.WriteLine($"YOU: {ToString(playerShape)} - {ToString(computerShape)} :COM");
+                Console.WriteLine($"{game.MatchResultMap[MatchResult.Win]} wins - {game.MatchResultMap[MatchResult.Tie]} ties - {game.MatchResultMap[MatchResult.Loss]} losses");
+            }
+        }
+
+        static string ToString(Shape shape)
+        {
+            switch (shape)
+            {
+                case Shape.Rock:
+                    return "Ｏ";
+                case Shape.Paper:
+                    return "Ｗ";
+                case Shape.Scissors:
+                    return "Ｖ";
+                default:
+                    throw new InvalidOperationException();
+            }
         }
     }
 }
