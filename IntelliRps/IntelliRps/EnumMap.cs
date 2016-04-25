aae3d0635f7b1b969e27796884f2daedfd04951c
Keiho Sakapon <saka_pon@live.jp>
2016-04-25 18:31:07 +0900

    Add classes

---
+++ IntelliRps/IntelliRps/EnumMap.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntelliRps
+{
+    public class EnumCountMap<TEnum>
+    {
+        public static readonly TEnum[] EnumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+
+        public int[] Counts { get; } = new int[EnumValues.Length];
+
+        public int this[TEnum key]
+        {
+            get { return Counts[Array.IndexOf(EnumValues, key)]; }
+            set { Counts[Array.IndexOf(EnumValues, key)] = value; }
+        }
+
+        public EnumCountMap()
+        {
+            if (!typeof(TEnum).IsEnum) throw new InvalidOperationException();
+        }
+
+        public EnumProbabilityMap<TEnum> GetProbability() => new EnumProbabilityMap<TEnum>(Counts);
+    }
+
+    public class EnumProbabilityMap<TEnum>
+    {
+        public static readonly TEnum[] EnumValues = (TEnum[])Enum.GetValues(typeof(TEnum));
+
+        public double[] Probabilities { get; }
+
+        public double this[TEnum key]
+        {
+            get { return Probabilities[Array.IndexOf(EnumValues, key)]; }
+        }
+
+        public EnumProbabilityMap(double[] ratio)
+        {
+            if (!typeof(TEnum).IsEnum) throw new InvalidOperationException();
+            if (ratio.Length != EnumValues.Length) throw new ArgumentException();
+
+            // Normalizes ratio.
+            var sum = ratio.Sum();
+            var probs =
+                sum == 0 ? Enumerable.Repeat(1.0 / EnumValues.Length, EnumValues.Length) :
+                ratio.Select(c => c / sum);
+            Probabilities = probs.ToArray();
+        }
+
+        public EnumProbabilityMap(int[] ratio)
+        {
+            if (!typeof(TEnum).IsEnum) throw new InvalidOperationException();
+            if (ratio.Length != EnumValues.Length) throw new ArgumentException();
+
+            // Normalizes ratio.
+            var sum = ratio.Sum();
+            var probs =
+                sum == 0 ? Enumerable.Repeat(1.0 / EnumValues.Length, EnumValues.Length) :
+                ratio.Select(c => c / (double)sum);
+            Probabilities = probs.ToArray();
+        }
+
+        public Dictionary<TEnum, double> ToDictionary() => Enumerable.Range(0, EnumValues.Length)
+            .ToDictionary(i => EnumValues[i], i => Probabilities[i]);
+
+        public TEnum NextRandomValue() => EnumValues[GameRules.NextIndex(Probabilities)];
+    }
+}
---
+++ IntelliRps/IntelliRps/Game.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IntelliRps
+{
+    public class Game
+    {
+        string ShapeHistory = "";
+        Dictionary<string, EnumCountMap<Shape>> ShapeHistoryMap = GameRules.CreateHistoryKeys().ToDictionary(k => k, _ => new EnumCountMap<Shape>());
+
+        public List<MatchInfo> MatchHistory { get; } = new List<MatchInfo>();
+        public EnumCountMap<MatchResult> MatchResultMap { get; } = new EnumCountMap<MatchResult>();
+
+        public Shape NextComputerShape()
+        {
+            var probs = GetProbabilities();
+
+            // --- Makes it hard for the player to win.
+            var d = probs.ToDictionary();
+            var top2 = d
+                .OrderByDescending(p => p.Value)
+                .Take(2)
+                .ToArray();
+            if (0.65 * top2[0].Value < top2[1].Value && top2[0].Value + top2[1].Value > 0.8)
+            {
+                var result = GameRules.GetResult(top2[0].Key, top2[1].Key);
+                return result == MatchResult.Win ? top2[0].Key : top2[1].Key;
+            }
+            // ---
+
+            var playerShape = probs.NextRandomValue();
+            return GameRules.GetDefeatingShape(playerShape);
+        }
+
+        public void AddMatchInfo(Shape playerShape, Shape computerShape)
+        {
+            var degrees = Enumerable.Range(0, GameRules.MaxHistoryDegree + 1)
+                .TakeWhile(d => ShapeHistory.Length >= d);
+            foreach (var d in degrees)
+            {
+                var key = ShapeHistory.Substring(ShapeHistory.Length - d, d);
+                ShapeHistoryMap[key][playerShape] += 1;
+            }
+
+            ShapeHistory += ((int)playerShape).ToString();
+
+            var match = new MatchInfo(playerShape, computerShape);
+            MatchHistory.Add(match);
+
+            MatchResultMap[match.PlayerResult] += 1;
+        }
+
+        EnumProbabilityMap<Shape> GetProbabilities0()
+        {
+            return ShapeHistoryMap[""].GetProbability();
+        }
+
+        EnumProbabilityMap<Shape> GetProbabilities()
+        {
+            if (ShapeHistory.Length == 0) return GetProbabilities0();
+
+            var degreeProbs = Enumerable.Range(0, GameRules.MaxHistoryDegree + 1)
+                .TakeWhile(d => ShapeHistory.Length >= d)
+                .Select(d => ShapeHistory.Substring(ShapeHistory.Length - d, d))
+                .Select(k => ShapeHistoryMap[k].GetProbability())
+                .ToArray();
+
+            var ratio = EnumProbabilityMap<Shape>.EnumValues
+                .Select(s => degreeProbs.Sum(r => r[s] * r[s]))
+                .ToArray();
+            return new EnumProbabilityMap<Shape>(ratio);
+        }
+    }
+
+    public class MatchInfo
+    {
+        public Shape PlayerShape { get; }
+        public Shape ComputerShape { get; }
+
+        public MatchResult PlayerResult => GameRules.GetResult(PlayerShape, ComputerShape);
+        public MatchResult ComputerResult => GameRules.GetResult(ComputerShape, PlayerShape);
+
+        public MatchInfo(Shape playerShape, Shape computerShape)
+        {
+            PlayerShape = playerShape;
+            ComputerShape = computerShape;
+        }
+    }
+}
