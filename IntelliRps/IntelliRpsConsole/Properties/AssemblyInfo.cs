063a94a03be7f10456f39292227985932628468c
Keiho Sakapon <saka_pon@live.jp>
2016-04-25 16:57:12 +0900

    Update project settings

--- IntelliRps/IntelliRpsConsole/Properties/AssemblyInfo.cs
+++ IntelliRps/IntelliRpsConsole/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("IntelliRpsConsole")]
+[assembly: AssemblyTitle("Intelli-RPS for Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IntelliRpsConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KTools")]
+[assembly: AssemblyCopyright("© 2016 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
