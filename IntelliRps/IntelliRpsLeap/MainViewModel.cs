97f4a07a0f62e237d5d7c0df78e7ba72ed0ee72c
Keiho Sakapon <saka_pon@live.jp>
2016-04-26 14:26:21 +0900

    Use FuncConverter

--- IntelliRps/IntelliRpsLeap/MainViewModel.cs
+++ IntelliRps/IntelliRpsLeap/MainViewModel.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using IntelliRps;
 using Reactive.Bindings;
 
 namespace IntelliRpsLeap
 {
     public class MainViewModel
     {
-        public AppModel AppModel { get; } = new AppModel();
+        public static readonly Func<RpsShape?, string> ToImagePath = s => s.HasValue ? $"Images/{s}-1.png" : null;
 
-        public ReadOnlyReactiveProperty<string> HandShapeImagePath { get; }
+        public AppModel AppModel { get; } = new AppModel();
 
         public MainViewModel()
         {
-            HandShapeImagePath = AppModel.HandShape
-                .Select(s => s.HasValue ? $"Images/{s}-1.png" : null)
-                .ToReadOnlyReactiveProperty();
         }
     }
 }
