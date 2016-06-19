02e5d3546b576b931e1aa7eb1572b157c92c7b64
Keiho Sakapon <saka_pon@live.jp>
2016-06-19 17:28:09 +0900

    Save strokes data

--- InkStrokes/InkStrokes/MainWindow.xaml.cs
+++ InkStrokes/InkStrokes/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +22,64 @@ namespace InkStrokes
     /// </summary>
     public partial class MainWindow : Window
     {
+        const double Radius = 4.0;
+        const string Dir_Images = "Images";
+        const string Dir_Data = "Data";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            TheInkCanvas.DefaultDrawingAttributes.Width = Radius;
+            TheInkCanvas.DefaultDrawingAttributes.Height = Radius;
+
+            Directory.CreateDirectory(Dir_Images);
+            Directory.CreateDirectory(Dir_Data);
+
+            ClearButton.Click += (o, e) =>
+            {
+                Clear();
+            };
+            SaveButton.Click += (o, e) =>
+            {
+                SaveStrokes();
+                Clear();
+            };
         }
+
+        void Clear()
+        {
+            TheInkCanvas.Strokes.Clear();
+        }
+
+        void SaveStrokes()
+        {
+            var fileName = CreateFileName();
+            var imagePath = $@"{Dir_Images}\{fileName}.png";
+            var dataPath = $@"{Dir_Data}\{fileName}.dat";
+
+            var bitmap = BitmapUtility.CreateImage(TheInkCanvas);
+            BitmapUtility.SaveImage(imagePath, bitmap);
+
+            var data = ToString(TheInkCanvas.Strokes);
+            File.WriteAllText(dataPath, data);
+        }
+
+        static string CreateFileName() =>
+            $"{DateTime.Now:yyyyMMdd-HHmmss}";
+
+        public const char StrokeDelimiter = '\n';
+        public const char PointDelimiter = ';';
+        public const char ElementDelimiter = ',';
+
+        static string ToString(StrokeCollection strokes) =>
+            string.Join(StrokeDelimiter.ToString(), strokes.Select(ToString));
+
+        static string ToString(Stroke stroke) =>
+            string.Join(PointDelimiter.ToString(), stroke.StylusPoints.Select(ToString));
+
+        static string ToString(StylusPoint p) =>
+            $"{p.X:F3}{ElementDelimiter}{p.Y:F3}";
+        //$"{p.X:F3}{ElementDelimiter}{p.Y:F3}{ElementDelimiter}{p.PressureFactor:F3}";
     }
 }
