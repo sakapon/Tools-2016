1a1cd1ea082e6081bbb81e384cdd08d395d5f220
Keiho Sakapon <saka_pon@live.jp>
2016-06-19 15:57:04 +0900

    Update project settings

--- InkStrokes/InkStrokes/Properties/AssemblyInfo.cs
+++ InkStrokes/InkStrokes/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("InkStrokes")]
+[assembly: AssemblyTitle("Ink Strokes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InkStrokes")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KTools")]
+[assembly: AssemblyCopyright("© 2016 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
