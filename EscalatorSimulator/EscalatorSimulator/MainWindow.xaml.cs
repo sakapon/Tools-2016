d2b16e5484f9ea8c0a5ff84b8771b6854269db74
Keiho Sakapon <git@saka-pon.net>
2016-10-14 15:42:15 +0900

    Update layout

--- EscalatorSimulator/EscalatorSimulator/MainWindow.xaml.cs
+++ EscalatorSimulator/EscalatorSimulator/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +25,45 @@ namespace EscalatorSimulator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void EscalatorPanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            var panel = (DockPanel)sender;
+            var escalator = (Escalator)panel.DataContext;
+            var canvas = (Canvas)panel.FindName("TheCanvas");
+
+            escalator.People1_L
+                .ObserveOn(Dispatcher)
+                .Subscribe(_ => SpawnCircle(canvas, escalator.Lane_L, 60));
+            escalator.People1_R
+                .ObserveOn(Dispatcher)
+                .Subscribe(_ => SpawnCircle(canvas, escalator.Lane_R, 120));
+        }
+
+        void SpawnCircle(Canvas canvas, Lane lane, double left)
+        {
+            var circle = new Ellipse();
+            Canvas.SetLeft(circle, left);
+            circle.Style = (Style)Resources["CircleStyle"];
+            circle.RenderTransform = new TranslateTransform();
+            canvas.Children.Add(circle);
+
+            var sb = CreateMovingStoryboard(lane, circle);
+            sb.Completed += (o, e) => canvas.Children.Remove(circle);
+            sb.Begin();
+        }
+
+        static Storyboard CreateMovingStoryboard(Lane lane, Ellipse circle)
+        {
+            var sb = new Storyboard();
+
+            var animation = lane.CreateAnimation();
+            Storyboard.SetTarget(animation, circle);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
+            sb.Children.Add(animation);
+
+            return sb;
         }
     }
 }
