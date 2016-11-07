36922a518740d0a145efffbdaa3dee25871b131c
Keiho Sakapon <git@saka-pon.net>
2016-11-07 18:39:25 +0900

    Add test

--- FileReplacer/UnitTest/DirectoryHelperTest.cs
+++ FileReplacer/UnitTest/DirectoryHelperTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using FileReplacer;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest
@@ -7,8 +9,32 @@ namespace UnitTest
     public class DirectoryHelperTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReplaceDirectoryName()
         {
+            if (Directory.Exists("Root"))
+                Directory.Delete("Root", true);
+            Directory.CreateDirectory(@"Root\OldDir");
+
+            var dir = new DirectoryInfo(@"Root\OldDir");
+            DirectoryHelper.ReplaceDirectoryName(dir, "Old", "New");
+
+            Assert.AreEqual(false, Directory.Exists(@"Root\OldDir"));
+            Assert.AreEqual(true, Directory.Exists(@"Root\NewDir"));
+        }
+
+        [TestMethod]
+        public void ReplaceFileName()
+        {
+            if (Directory.Exists("Root"))
+                Directory.Delete("Root", true);
+            Directory.CreateDirectory("Root");
+            File.WriteAllText(@"Root\OldFile.txt", "Text");
+
+            var file = new FileInfo(@"Root\OldFile.txt");
+            DirectoryHelper.ReplaceFileName(file, "Old", "New");
+
+            Assert.AreEqual(false, File.Exists(@"Root\OldFile.txt"));
+            Assert.AreEqual(true, File.Exists(@"Root\NewFile.txt"));
         }
     }
 }
