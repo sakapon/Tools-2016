cf58549cfbabe86580870809ad760b7b5a40c299
Keiho Sakapon <git@saka-pon.net>
2016-11-07 16:27:01 +0900

    Add test

--- FileReplacer/UnitTest/DirectoryHelper.cs
+++ FileReplacer/UnitTest/DirectoryHelper.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class DirectoryHelper
     {
         [TestMethod]
         public void TestMethod1()
